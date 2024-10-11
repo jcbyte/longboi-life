636dd8f87c035d6502093e0b70067664bb93e931
Joel Cutler <joelcutler108@gmail.com>
2024-10-11 18:48:49 +0100

    created tile manager similar to building manager class for retrieving data

---
+++ core/src/main/java/com/spacecomplexity/longboilife/TileData.java
@@ -0,0 +1,11 @@
+package com.spacecomplexity.longboilife;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class TileData {
+    Texture tex;
+
+    public TileData(Texture tex) {
+        this.tex = tex;
+    }
+}
---
+++ core/src/main/java/com/spacecomplexity/longboilife/TileManager.java
@@ -0,0 +1,27 @@
+package com.spacecomplexity.longboilife;
+
+import com.badlogic.gdx.graphics.Texture;
+
+import java.util.EnumMap;
+
+public class TileManager {
+    private final EnumMap<TileType, TileData> tileDescriptions;
+
+    public TileManager() {
+        tileDescriptions = new EnumMap<>(TileType.class);
+
+        tileDescriptions.put(TileType.GRASS, new TileData(
+            new Texture("tiles/grass.png")
+        ));
+        // todo this with all buildings
+    }
+
+    public TileData getTileData(TileType tileType) throws ClassNotFoundException {
+        TileData data = tileDescriptions.get(tileType);
+        if (data == null) {
+            throw new ClassNotFoundException();
+        }
+
+        return data;
+    }
+}
