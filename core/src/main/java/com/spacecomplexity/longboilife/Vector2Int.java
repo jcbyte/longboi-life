32eaadc31ab7381ca00dae1dd5ecf9f560573a8d
Joel Cutler <joelcutler108@gmail.com>
2024-10-11 18:02:53 +0100

    added Vector2Int class

---
+++ core/src/main/java/com/spacecomplexity/longboilife/Vector2Int.java
@@ -0,0 +1,11 @@
+package com.spacecomplexity.longboilife;
+
+public class Vector2Int {
+    public int x;
+    public int y;
+
+    Vector2Int(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
