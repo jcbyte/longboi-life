049f96cab5b6af28f32d0eec57763e3d24e9a17b
Joel Cutler <joelcutler108@gmail.com>
2024-10-12 16:04:07 +0100

    added an unknown tile for errors

---
+++ core/src/main/java/com/spacecomplexity/longboilife/tile/UnknownTile.java
@@ -0,0 +1,9 @@
+package com.spacecomplexity.longboilife.tile;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class UnknownTile extends Tile {
+    public UnknownTile() {
+        super(new Texture("error.png"), false);
+    }
+}
