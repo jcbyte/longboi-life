e22cff696f85f335d9dbb686069a7e9341c35d1c
Joel Cutler <joelcutler108@gmail.com>
2024-10-12 16:18:25 +0100

    moved drawing grid into static function in RenderUtils

--- core/src/main/java/com/spacecomplexity/longboilife/Main.java
+++ core/src/main/java/com/spacecomplexity/longboilife/Main.java
@@ -3,6 +3,7 @@ package com.spacecomplexity.longboilife;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.spacecomplexity.longboilife.utils.RenderUtils;
 
 import java.io.FileNotFoundException;
 
@@ -11,8 +12,6 @@ import java.io.FileNotFoundException;
  * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms.
  */
 public class Main extends ApplicationAdapter {
-    final int TILE_SIZE = 20; // in pixels
-
     private SpriteBatch batch;
     private World world;
 
@@ -31,14 +30,7 @@ public class Main extends ApplicationAdapter {
     public void render() {
         ScreenUtils.clear(0, 0, 0, 1f);
         batch.begin();
-
-
-        for (int x = 0; x < world.size.x; x++) {
-            for (int y = 0; y < world.size.y; y++) {
-                batch.draw(world.world[x][y].getTex(), x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-            }
-        }
-
+        RenderUtils.drawWorld(batch, world);
         batch.end();
     }
 
---
+++ core/src/main/java/com/spacecomplexity/longboilife/utils/RenderUtils.java
@@ -0,0 +1,17 @@
+package com.spacecomplexity.longboilife.utils;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.spacecomplexity.longboilife.World;
+
+public class RenderUtils {
+    // todo should TILE_SIZE be here?
+    public static final int TILE_SIZE = 20; // in pixels
+
+    public static void drawWorld(SpriteBatch batch, World world) {
+        for (int x = 0; x < world.size.x; x++) {
+            for (int y = 0; y < world.size.y; y++) {
+                batch.draw(world.world[x][y].getTex(), x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+            }
+        }
+    }
+}
