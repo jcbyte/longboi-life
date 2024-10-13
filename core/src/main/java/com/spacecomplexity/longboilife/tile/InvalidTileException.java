a7211519ab54945f7d66f7c2f24549a75ca6850d
Joel Cutler <joelcutler108@gmail.com>
2024-10-13 18:34:18 +0100

    documented tile classes

--- core/src/main/java/com/spacecomplexity/longboilife/tile/InvalidTileException.java
+++ core/src/main/java/com/spacecomplexity/longboilife/tile/InvalidTileException.java
@@ -1,5 +1,8 @@
 package com.spacecomplexity.longboilife.tile;
 
+/**
+ * Exception to indicate an invalid tile was found.
+ */
 public class InvalidTileException extends RuntimeException {
     public InvalidTileException(String message) {
         super(message);
--- core/src/main/java/com/spacecomplexity/longboilife/tile/Tile.java
+++ core/src/main/java/com/spacecomplexity/longboilife/tile/Tile.java
@@ -2,27 +2,56 @@ package com.spacecomplexity.longboilife.tile;
 
 import com.badlogic.gdx.graphics.Texture;
 
+/**
+ * Represents a generic tile in the game.
+ * <p>
+ * All subclasses should be implemented as singleton classes.
+ */
 public abstract class Tile {
     private final Texture texture;
     private final boolean isBuildable;
 
+    /**
+     * Constructs a tile instance with specified attributes.
+     *
+     * @param texture    the texture associated with this tile.
+     * @param canBuildOn whether the tile can be built on.
+     */
     protected Tile(Texture texture, boolean canBuildOn) {
         this.texture = texture;
         this.isBuildable = canBuildOn;
     }
 
+    /**
+     * Returns the singleton instance of the implemented class.
+     *
+     * @return the single instance of the implemented class.
+     */
     public static Tile getInstance() {
         throw new UnsupportedOperationException("Method should be implemented in subclasses as all subclasses should be singletons");
     }
 
+    /**
+     * Dispose of resources allocated with this tile.
+     */
     public void dispose() {
         texture.dispose();
     }
 
+    /**
+     * Get the texture associated with the tile.
+     *
+     * @return the associated texture.
+     */
     public Texture getTexture() {
         return texture;
     }
 
+    /**
+     * Get whether the tile is buildable.
+     *
+     * @return whether the tile is buildable.
+     */
     public boolean isBuildable() {
         return isBuildable;
     }
