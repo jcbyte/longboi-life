44e2db28ef60c821a323dbd2577aca1d0cf58d60
Joel Cutler <joelcutler108@gmail.com>
2024-10-13 18:10:44 +0100

    documented and refactored map/world classes

--- core/src/main/java/com/spacecomplexity/longboilife/world/SaveMap.java
+++ core/src/main/java/com/spacecomplexity/longboilife/world/SaveMap.java
@@ -5,32 +5,53 @@ import com.spacecomplexity.longboilife.tile.InvalidTileException;
 import com.spacecomplexity.longboilife.tile.Tile;
 import com.spacecomplexity.longboilife.tile.WaterTile;
 
+/**
+ * Class representing the saved map in a JSON format.
+ * <p>
+ * JSON format:
+ * <pre>
+ * {
+ *  "map": [
+ *      ["GRASS", "GRASS", "GRASS"],
+ *      ["WATER", "WATER", "GRASS"]
+ *  ]
+ * }
+ * </pre>
+ * Where each element can be any valid tile name.
+ */
 public class SaveMap {
     private String[][] map;
 
     public SaveMap() {
     }
 
+    /**
+     * Transforms the JSON string grid into a tile grid which {@link World} requires.
+     *
+     * @return the 2D {@link Tile} grid representing the world.
+     * @throws InvalidTileException if a tile given is not valid name assigned.
+     */
     public Tile[][] getWorld() throws InvalidTileException {
+        // Grid generated will be transformed so it can be accessed as map[x][y]
+        // y will also be flipped as libGDX coordinates start in the bottom right instead of left
 
         int height = map.length;
         int width = map[0].length;
 
+        // Initialise the array with given width and height
         Tile[][] world = new Tile[width][height];
 
+        // Go though each element in the object in the specified order
         for (int y = height - 1; y >= 0; y--) {
             for (int x = 0; x < width; x++) {
-                Tile tile = null;
+                // Determine the type of tile based on value in the JSON map
+                Tile tile = switch (map[y][x]) {
+                    case "GRASS" -> GrassTile.getInstance();
+                    case "WATER" -> WaterTile.getInstance();
+                    default -> null;
+                };
 
-                switch (map[y][x]) {
-                    case "GRASS":
-                        tile = GrassTile.getInstance();
-                        break;
-                    case "WATER":
-                        tile = WaterTile.getInstance();
-                        break;
-                }
-
+                // If the tile is invalid throw an error
                 if (tile == null) {
                     throw new InvalidTileException("Invalid tile: " + map[x][y]);
                 }
--- core/src/main/java/com/spacecomplexity/longboilife/world/World.java
+++ core/src/main/java/com/spacecomplexity/longboilife/world/World.java
@@ -10,35 +10,73 @@ import com.spacecomplexity.longboilife.tile.Tile;
 import java.io.FileNotFoundException;
 import java.util.Vector;
 
+/**
+ * Represents the game world containing the base tiles and buildings.
+ */
 public class World {
     private Tile[][] world;
     public Vector<PlacedBuilding> buildings;
 
+    /**
+     * Creates a new world loaded from a map JSON file.
+     *
+     * @param filename the name of the JSON file.
+     * @throws FileNotFoundException if the specified file does not exist.
+     * @throws InvalidTileException  if the map contains invalid tile names.
+     */
     public World(String filename) throws FileNotFoundException, InvalidTileException {
-        loadMap(filename);
+        world = getMap(filename);
         buildings = new Vector<>();
     }
 
-    private void loadMap(String filename) throws FileNotFoundException, InvalidTileException {
+    /**
+     * Loads the map from JSON file and returns the tile grid.
+     *
+     * @param filename the name of the JSON file.
+     * @return the {@link Tile} grid representing the world extracted from the JSON file.
+     * @throws FileNotFoundException if the specified file does not exist.
+     * @throws InvalidTileException  if the map contains invalid tile names.
+     */
+    private Tile[][] getMap(String filename) throws FileNotFoundException, InvalidTileException {
         Json json = new Json();
         FileHandle file = Gdx.files.local(filename);
 
+        // If the file does not exist throw an exception
         if (!file.exists()) {
             throw new FileNotFoundException("File does not exist: " + filename);
         }
 
+        // Deserialize the JSON data into the SaveMap object
         SaveMap saveMap = json.fromJson(SaveMap.class, file.readString());
-        world = saveMap.getWorld();
+        // Return the Tile[][] from this object.
+        return saveMap.getWorld();
     }
 
+    /**
+     * Retrieves the tile at the specified coordinates.
+     *
+     * @param x the x coordinate.
+     * @param y the y coordinate.
+     * @return the {@link Tile} at the specified coordinates.
+     */
     public Tile getTile(int x, int y) {
         return world[x][y];
     }
 
+    /**
+     * Get the height of the world.
+     *
+     * @return the height of the world.
+     */
     public int getHeight() {
         return world[0].length;
     }
 
+    /**
+     * Get the width of the world.
+     *
+     * @return the width of the world.
+     */
     public int getWidth() {
         return world.length;
     }
