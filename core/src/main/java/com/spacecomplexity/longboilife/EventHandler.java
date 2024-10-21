c6cc18144d5203cb904e4baa818bc327945f62d9
Joel Cutler <joelcutler108@gmail.com>
2024-10-21 19:39:13 +0100

    allow event handler to give parameters

--- core/src/main/java/com/spacecomplexity/longboilife/EventHandler.java
+++ core/src/main/java/com/spacecomplexity/longboilife/EventHandler.java
@@ -1,14 +1,14 @@
 package com.spacecomplexity.longboilife;
 
 import java.util.HashMap;
-import java.util.concurrent.Callable;
+import java.util.function.Function;
 
 /**
  * CLass to manage events which can be called from anywhere within the game.
  */
 public class EventHandler {
     private static final EventHandler eventHandler = new EventHandler();
-    private HashMap<String, Callable<?>> events;
+    private HashMap<String, Function<Object[], Object>> events;
 
     /**
      * Initialise events attributes.
@@ -23,20 +23,20 @@ public class EventHandler {
      * @param name  the name of the event, needed when called.
      * @param event the event method, this is what will be executed.
      */
-    public void createEvent(String name, Callable<?> event) {
+    public void createEvent(String name, Function<Object[], Object> event) {
         events.put(name, event);
     }
 
     /**
      * Call a previously defined event.
      *
-     * @param name the name of the event defined.
+     * @param name   the name of the event defined.
+     * @param params the parameter to pass to the event.
      * @return what the original event would return, this will need to be cast as we cannot know the type here.
      * @throws NoSuchMethodException if the event has not been defined.
-     * @throws Exception             if the event throws an exception.
      */
-    public Object callEvent(String name) throws NoSuchMethodException, Exception {
-        Callable<?> event = events.get(name);
+    public Object callEvent(String name, Object... params) throws NoSuchMethodException {
+        Function<Object[], Object> event = events.get(name);
 
         // If the event is not defined then throw an error
         if (event == null) {
@@ -44,7 +44,7 @@ public class EventHandler {
         }
 
         // Execute the event and return the result
-        return event.call();
+        return event.apply(params);
     }
 
     /**
