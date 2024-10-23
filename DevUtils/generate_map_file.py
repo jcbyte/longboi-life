f6af45ea99921bf1730f31eb8d1071ed264db146
Joel Cutler <joelcutler108@gmail.com>
2024-10-23 16:04:07 +0100

    updated generate_map_file.py dev util to take command line parameters

--- DevUtils/generate_map_file.py
+++ DevUtils/generate_map_file.py
@@ -1,28 +1,39 @@
+import argparse
 import json
 
 from PIL import Image
 
-IMG_MAP_PATH = "map.png"
-
 COLOUR_MAPPINGS = {(0, 255, 0): "GRASS", (0, 0, 255): "WATER"}
 
-# Open the image file
-image = Image.open(IMG_MAP_PATH)
-image = image.convert("RGB")
 
-width, height = image.size
+def create_map(in_img, out_json):
+    # Open the image file
+    image = Image.open(in_img)
+    image = image.convert("RGB")
 
-extracted_map = []
+    width, height = image.size
 
-# Iterate through each pixel going from bottom right
-for y in range(height):
-    extracted_map.append([])
+    extracted_map = []
 
-    for x in range(width):
-        # Add the tile with specified mapping to the array
-        col = image.getpixel((x, y))
-        extracted_map[y].append(COLOUR_MAPPINGS[col])
+    # Iterate through each pixel going from bottom right
+    for y in range(height):
+        extracted_map.append([])
 
-# Write the map into a file
-with open("map.json", "w") as f:
-    json.dump({"map": extracted_map}, f)
+        for x in range(width):
+            # Add the tile with specified mapping to the array
+            col = image.getpixel((x, y))
+            extracted_map[y].append(COLOUR_MAPPINGS[col])
+
+    # Write the map into a file
+    with open(out_json, "w") as f:
+        json.dump({"map": extracted_map}, f)
+
+
+# Parse arguments to get the input image file and output json file names
+parser = argparse.ArgumentParser()
+parser.add_argument("input_path", type=str, help="Input Image Path")
+parser.add_argument("-o", "--out", type=str, help="Output JSON Path", default="map.json")
+args = parser.parse_args()
+
+
+create_map(args.input_path, args.out)
