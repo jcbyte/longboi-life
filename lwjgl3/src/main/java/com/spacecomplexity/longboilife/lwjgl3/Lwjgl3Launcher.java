d19c2c6d0f4c545e35185dd3814f62931faf53db
Joel Cutler <joelcutler108@gmail.com>
2024-11-01 20:50:42 +0000

    added icon

--- lwjgl3/src/main/java/com/spacecomplexity/longboilife/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/spacecomplexity/longboilife/lwjgl3/Lwjgl3Launcher.java
@@ -32,7 +32,7 @@ public class Lwjgl3Launcher {
         configuration.setWindowedMode(640, 480);
         configuration.setWindowSizeLimits(640, 480, 99999, 99999);
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        configuration.setWindowIcon("icon128.png", "icon64.png", "icon32.png", "icon16.png");
         return configuration;
     }
 }
